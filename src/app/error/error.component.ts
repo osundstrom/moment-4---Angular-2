625782fc0f59fbc164ffefac28a14d539f5306f3
Oskar Sundström <ossu2300@student.miun.se>
2024-04-25 17:30:48 +0200

    updated error page

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './error.component.html',
   styleUrl: './error.component.scss'
 })
 export class ErrorComponent {
-
+  errorImage: string = "/assets/images/errorPhone.jpg";
 }
